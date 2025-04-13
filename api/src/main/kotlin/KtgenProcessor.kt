29f658dcaf33d1d6fa5e56da80f7cac90d02f574
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-04-12 20:31:47 -0400

    Only deleted unupdated file

--- api/src/main/kotlin/KtgenProcessor.kt
+++ api/src/main/kotlin/KtgenProcessor.kt
@@ -2,6 +2,18 @@ package net.echonolix.ktgen
 
 import java.nio.file.Path
 
+/**
+ * Interface for processing files in the ktgen runtime.
+ *
+ * This interface is used to define a processor that can process input files and generate output files.
+ * The processor is expected to be registered as a service in the `META-INF/services` directory.
+ */
 interface KtgenProcessor {
-    fun process(inputs: List<Path>, outputDir: Path)
+    /**
+     * @param inputs Input file paths
+     * @param outputDir Output directory
+     *
+     * @return Updated file paths
+     */
+    fun process(inputs: Set<Path>, outputDir: Path): Set<Path>
 }
--- runtime/src/main/kotlin/KtgenRuntime.kt
+++ runtime/src/main/kotlin/KtgenRuntime.kt
@@ -1,21 +1,45 @@
 package net.echonolix.ktgen
 
 import java.io.File
+import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.*
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.PathWalkOption
+import kotlin.io.path.deleteExisting
+import kotlin.io.path.deleteRecursively
+import kotlin.io.path.fileSize
+import kotlin.io.path.isDirectory
+import kotlin.io.path.walk
 
 object KtgenRuntime {
+    tailrec fun addParentUpTo(curr: Path?, end: Path, output: MutableCollection<Path>) {
+        if (curr == null) return
+        if (curr == end) return
+        output.add(curr)
+        return addParentUpTo(curr.parent, end, output)
+    }
+    @OptIn(ExperimentalPathApi::class)
     @JvmStatic
     fun main(args: Array<String>) {
-        val inputs = args[1].split(File.pathSeparator).map { Paths.get(it) }
+        val inputs = args[1].split(File.pathSeparator).mapTo(mutableSetOf()) { Paths.get(it) }
         val outputDir = Paths.get(args[0])
-        with(outputDir.toFile()) {
-            deleteRecursively()
-            mkdirs()
-        }
+        val outputDirFile = outputDir.toFile()
+        outputDirFile.mkdirs()
         val services = ServiceLoader.load(KtgenProcessor::class.java)
+        val updatedFiles = mutableSetOf<Path>()
         for (service in services) {
-            service.process(inputs, outputDir)
+            updatedFiles += service.process(inputs, outputDir)
         }
+        updatedFiles.toList()
+            .forEach {
+                addParentUpTo(it.parent, outputDir, updatedFiles)
+            }
+        outputDir.walk(PathWalkOption.INCLUDE_DIRECTORIES, PathWalkOption.BREADTH_FIRST, PathWalkOption.FOLLOW_LINKS)
+            .filter { it != outputDir }
+            .filter { it !in updatedFiles }
+            .forEach {
+                it.deleteRecursively()
+            }
     }
 }
